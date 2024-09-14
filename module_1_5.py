02046e1c116dc95a7431e2c23bf7b0005a445e8d
g4rder <ilyagarder@mail.ru>
2024-09-14 11:12:28 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 0, 4, 0, 4, 2, 0, 0, 0
+print(immutable_var)
+
+mutable_list = ['book', 'laptop','dog', 'pen', 'smartphone']
+print(mutable_list)
+mutable_list[2] = 'helicopter'
+print(mutable_list)
+mutable_list[0] = [100, 300]
+print(mutable_list)
+mutable_list[0][1] = 200
+print(mutable_list)
